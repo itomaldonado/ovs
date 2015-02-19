07388899ea2ee722bb2eee351f8617d205a0e9c5
itomaldonado <mo.maldonado@gmail.com>
2015-02-19 11:28:07 -0500

    Kick-off Drone again...

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request, abort, make_response, render_template
-import uuid
+from datetime import datetime, timedelta
+import uuid, re
 
 # Flask light-weight web server.
 server = Flask(__name__)
@@ -7,12 +8,8 @@ server = Flask(__name__)
 
 # In-memory Databases - Dynamic Dictionaries
 orders = dict()
-products = ['FiOS','SONET', 'VOD']
-bad_countries = []
-bad_states = []
-
-
-
+products = ['FiOS','SONET', 'VOD', 'NEW_PROD']
+bad_states = ['CA','TX','FL']
 
 ##########################
 ##### REST SERVICES ######
@@ -52,11 +49,13 @@ def post_order():
         new_order = dict()
         new_order['name'] = request.form['name']
         new_order['address'] = request.form['address']
-        new_order['productType'] = request.form['productType']
+        new_order['city'] = request.form['city']
+        new_order['state'] = request.form['state']
+        new_order['zipcode'] = request.form['zipcode']
+        new_order['dueDate'] = request.form['dueDate']
 
     # validate order
     valid,error_msg = order_field_validation(new_order)
-    # If not valid, return status 400 with a json body containing the error message.
     if not valid:
         return jsonify({'error': error_msg}), 400
 
@@ -79,12 +78,57 @@ def post_order():
 # Order Validation, returns a tuple, (boolean,string)
 def order_field_validation(order={}):
 
-    if order:
-        # Returns (True,'') if order is not empty.
-        return (True,'')
+    valid,error = validate_empty_order(order=order)
+    if not valid:
+        return valid, error
+
+    valid,error = validate_states(order=order)
+    if not valid:
+        return valid, error
+
+    valid,error = validate_due_date(order=order)
+    if not valid:
+        return valid, error
+
+    valid,error = validate_zipcodes(order=order)
+    if not valid:
+        return valid, error
+
+    return True, ''
+
+# CHeck if the order is not empty.
+def validate_empty_order(order={}):
+    if not order:
+        return False, 'order is empty'
     else:
-        # Return (False,'order is empty') if order is empty
-        return (False,'order is empty')
+        return True, ''
+
+# Check if the state is not part of the bad states
+def validate_states(order={}):
+    if order['state'] in bad_states:
+        return False, 'state not in service'
+    else:
+        return True, ''
+
+# Check if the due date is not too early (or in the past!)
+def validate_due_date(order={}):
+    if (datetime.strptime(order['dueDate'],"%m/%d/%Y") - datetime.now()).days < 5:
+        return False, 'due date is too early'
+    else:
+        return True, ''
+
+# Check if the zipcode is valid
+def validate_zipcodes(order={}):
+    if re.match(r'(^[0-9]{5}-?[0-9]{4})$', order['zipcode']):
+        return False, 'no support for zip+4'
+    elif not re.match(r'^[0-9]+$', order['zipcode']):
+        return False,'US zipcodes only contain digits'
+    elif (not re.match('^[0-9]{5}$',order['zipcode']))\
+            or (int(order['zipcode']) < 601)\
+            or (int(order['zipcode']) > 99950):
+        return False,'invalid zipcode'
+    else:
+        return True, ''
 
 
 
--- test/test_app.py
+++ test/test_app.py
@@ -1,5 +1,6 @@
 import unittest
-import json
+import json, re
+from datetime import datetime, timedelta
 from app import server
 from flask import jsonify
 
@@ -11,35 +12,35 @@ class OVSTest(unittest.TestCase):
 
     def test_root(self):
         result = self.applications.get('/')
-        assert 'OK' == result.data
+        assert 200 == result.status_code
         
     def test_valid_orders(self):
         # Testing productType: SONET
-        order = '{"name": "John Smith", "address": "One Verizon Way, Basking Ridge, NJ, 07920", "productType": "SONET"}'
+        dueDate = datetime.today() + timedelta(days=6)
+        order = '{"name": "John Smith", ' \
+                '"address": "One Verizon Way", ' \
+                '"city": "Basking Ridge", ' \
+                '"state": "NJ", ' \
+                '"zipcode": "07920", ' \
+                '"productType": "SONET", ' \
+                '"dueDate": "'+dueDate.strftime("%m/%d/%Y")+'"}'
         result = self.applications.post('/ovs/orders', content_type='application/json', data=order)
         json_result = json.loads(result.data)
         assert 200 == result.status_code
 
         # Testing productType: FiOS
-        order = '{"name": "John Smith", "address": "One Verizon Way, Basking Ridge, NJ, 07920", "productType": "FiOS"}'
+        dueDate = datetime.now() + timedelta(days=6)
+        order = '{"name": "John Smith", "address": "One Verizon Way", "city": "Basking Ridge", "state": "NJ", "zipcode": "07920", "productType": "FiOS", "dueDate": "'+dueDate.strftime("%m/%d/%Y")+'"}'
         result = self.applications.post('/ovs/orders', content_type='application/json', data=order)
         json_result = json.loads(result.data)
         # Check Results
         assert 200 == result.status_code
 
-    def test_invalid_orders(self):
-        # Testing empty order
-        order = '{}'
-        result = self.applications.post('/ovs/orders', content_type='application/json', data=order)
-        json_result = json.loads(result.data)
-        # Check Results
-        assert 400 == result.status_code
-        assert 'error' in json_result
-        assert 'order is empty' == json_result['error']
 
     def test_order_creation(self):
         # Create order
-        order = '{"name": "John Smith", "address": "One Verizon Way, Basking Ridge, NJ, 07920", "productType": "FiOS"}'
+        dueDate = datetime.now() + timedelta(days=6)
+        order = '{"name": "John Smith", "address": "One Verizon Way", "city": "Basking Ridge", "state": "NJ", "zipcode": "07920", "productType": "SONET", "dueDate": "'+dueDate.strftime("%m/%d/%Y")+'"}'
         result_post = self.applications.post('/ovs/orders', content_type='application/json', data=order)
         json_result_post = json.loads(result_post.data)
         # Check Results from POST
@@ -53,12 +54,105 @@ class OVSTest(unittest.TestCase):
 
 
 
-    def test_invalid_orders(self):
+    # Testing Invalid Scenarios!
+
+    def test_empty_order(self):
         # Testing empty order
-        order = '{}'
+        new_order = '{}'
+        result = self.applications.post('/ovs/orders', content_type='application/json', data=new_order)
+        json_result = json.loads(result.data)
+        # Check Results
+        assert 400 == result.status_code
+        assert 'error' in json_result
+        assert 'order is empty' == json_result['error']
+
+    def test_invalid_due_date(self):
+        # Right now
+        dueDate = datetime.now()
+        order = '{"name": "John Smith", "address": "One Verizon Way", "city": "Basking Ridge", "state": "NJ", "zipcode": "07920", "productType": "SONET", "dueDate": "'+dueDate.strftime("%m/%d/%Y")+'"}'
         result = self.applications.post('/ovs/orders', content_type='application/json', data=order)
         json_result = json.loads(result.data)
-        assert 400 == result.status_code and 'error' in json_result and 'order is empty' == json_result['error']
+        assert 400 == result.status_code and 'error' in json_result and 'due date is too early' == json_result['error']
+
+        # 2 days from now
+        dueDate = datetime.now() + timedelta(days=2)
+        order = '{"name": "John Smith", "address": "One Verizon Way", "city": "Basking Ridge", "state": "NJ", "zipcode": "07920", "productType": "SONET", "dueDate": "'+dueDate.strftime("%m/%d/%Y")+'"}'
+        result = self.applications.post('/ovs/orders', content_type='application/json', data=order)
+        json_result = json.loads(result.data)
+        assert 400 == result.status_code and 'error' in json_result and 'due date is too early' == json_result['error']
+
+    def test_invalid_state(self):
+        # FL
+        dueDate = datetime.now()
+        order = '{"name": "John Smith", "address": "One Verizon Way", "city": "Basking Ridge", "state": "FL", "zipcode": "07920", "productType": "SONET", "dueDate": "'+dueDate.strftime("%m/%d/%Y")+'"}'
+        result = self.applications.post('/ovs/orders', content_type='application/json', data=order)
+        json_result = json.loads(result.data)
+        assert 400 == result.status_code and 'error' in json_result and 'state not in service' == json_result['error']
+
+        # CA
+        dueDate = datetime.now()
+        order = '{"name": "John Smith", "address": "One Verizon Way", "city": "Basking Ridge", "state": "CA", "zipcode": "07920", "productType": "SONET", "dueDate": "'+dueDate.strftime("%m/%d/%Y")+'"}'
+        result = self.applications.post('/ovs/orders', content_type='application/json', data=order)
+        json_result = json.loads(result.data)
+        assert 400 == result.status_code and 'error' in json_result and 'state not in service' == json_result['error']
+
+        # TX
+        dueDate = datetime.now()
+        order = '{"name": "John Smith", "address": "One Verizon Way", "city": "Basking Ridge", "state": "TX", "zipcode": "07920", "productType": "SONET", "dueDate": "'+dueDate.strftime("%m/%d/%Y")+'"}'
+        result = self.applications.post('/ovs/orders', content_type='application/json', data=order)
+        json_result = json.loads(result.data)
+        assert 400 == result.status_code and 'error' in json_result and 'state not in service' == json_result['error']
+
+
+    def test_invalid_zipcodes(self):
+        # 4 digits
+        dueDate = datetime.now() + timedelta(days=6)
+        order = '{"name": "John Smith", "address": "One Verizon Way", "city": "Basking Ridge", "state": "NJ", "zipcode": "0000", "productType": "SONET", "dueDate": "'+dueDate.strftime("%m/%d/%Y")+'"}'
+        result = self.applications.post('/ovs/orders', content_type='application/json', data=order)
+        json_result = json.loads(result.data)
+        assert 400 == result.status_code and 'error' in json_result and 'invalid zipcode' == json_result['error']
+
+        # under the max
+        dueDate = datetime.now() + timedelta(days=6)
+        order = '{"name": "John Smith", "address": "One Verizon Way", "city": "Basking Ridge", "state": "NJ", "zipcode": "00600", "productType": "SONET", "dueDate": "'+dueDate.strftime("%m/%d/%Y")+'"}'
+        result = self.applications.post('/ovs/orders', content_type='application/json', data=order)
+        json_result = json.loads(result.data)
+        assert 400 == result.status_code and 'error' in json_result and 'invalid zipcode' == json_result['error']
+
+        # Over the max
+        dueDate = datetime.now() + timedelta(days=6)
+        order = '{"name": "John Smith", "address": "One Verizon Way", "city": "Basking Ridge", "state": "NJ", "zipcode": "99951", "productType": "SONET", "dueDate": "'+dueDate.strftime("%m/%d/%Y")+'"}'
+        result = self.applications.post('/ovs/orders', content_type='application/json', data=order)
+        json_result = json.loads(result.data)
+        assert 400 == result.status_code and 'error' in json_result and 'invalid zipcode' == json_result['error']
+
+        # 6 digits
+        dueDate = datetime.now() + timedelta(days=6)
+        order = '{"name": "John Smith", "address": "One Verizon Way", "city": "Basking Ridge", "state": "NJ", "zipcode": "999999", "productType": "SONET", "dueDate": "'+dueDate.strftime("%m/%d/%Y")+'"}'
+        result = self.applications.post('/ovs/orders', content_type='application/json', data=order)
+        json_result = json.loads(result.data)
+        assert 400 == result.status_code and 'error' in json_result and 'invalid zipcode' == json_result['error']
+
+        # zip+4
+        dueDate = datetime.now() + timedelta(days=6)
+        order = '{"name": "John Smith", "address": "One Verizon Way", "city": "Basking Ridge", "state": "NJ", "zipcode": "00000-0000", "productType": "SONET", "dueDate": "'+dueDate.strftime("%m/%d/%Y")+'"}'
+        result = self.applications.post('/ovs/orders', content_type='application/json', data=order)
+        json_result = json.loads(result.data)
+        assert 400 == result.status_code and 'error' in json_result and 'no support for zip+4' == json_result['error']
+
+        # zip+4 no dash
+        dueDate = datetime.now() + timedelta(days=6)
+        order = '{"name": "John Smith", "address": "One Verizon Way", "city": "Basking Ridge", "state": "NJ", "zipcode": "000000000", "productType": "SONET", "dueDate": "'+dueDate.strftime("%m/%d/%Y")+'"}'
+        result = self.applications.post('/ovs/orders', content_type='application/json', data=order)
+        json_result = json.loads(result.data)
+        assert 400 == result.status_code and 'error' in json_result and 'no support for zip+4' == json_result['error']
+
+        # not only numbers
+        dueDate = datetime.now() + timedelta(days=6)
+        order = '{"name": "John Smith", "address": "One Verizon Way", "city": "Basking Ridge", "state": "NJ", "zipcode": "09asd", "productType": "SONET", "dueDate": "'+dueDate.strftime("%m/%d/%Y")+'"}'
+        result = self.applications.post('/ovs/orders', content_type='application/json', data=order)
+        json_result = json.loads(result.data)
+        assert 400 == result.status_code and 'error' in json_result and 'US zipcodes only contain digits' == json_result['error']
 
 if __name__ == '__main__':
     unittest.main()
